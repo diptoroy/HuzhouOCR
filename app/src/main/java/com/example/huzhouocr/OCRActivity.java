891a76862e00312aea7072612ef65df15f47da17
diptoroy <diptoroy54@gmail.com>
2020-01-17 20:51:29 +0800

    another commit

--- app/src/main/java/com/example/huzhouocr/OCRActivity.java
+++ app/src/main/java/com/example/huzhouocr/OCRActivity.java
@@ -1,14 +1,77 @@
 package com.example.huzhouocr;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class OCRActivity extends AppCompatActivity {
+import com.theartofdev.edmodo.cropper.CropImage;
+
+public class OCRActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private ImageView selectCamera,selectGallery;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocr);
+
+        init();
+    }
+
+    private void init() {
+
+        selectCamera = findViewById(R.id.camera);
+        selectGallery = findViewById(R.id.gallery);
+
+        selectGallery.setOnClickListener(this);
+        selectCamera.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.camera:
+                sCamera();
+                break;
+
+            case R.id.gallery:
+                sGallery();
+                break;
+
+            default:
+        }
+
+    }
+
+    private void sGallery() {
+
+        //gallery code here
+        CropImage.startPickImageActivity(OCRActivity.this);
+    }
+
+    private void sCamera() {
+
+        //camera code here
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                uri = result.getUri();
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
