2535cdb4d8a021d5a2796a1f2cff01adfe2a954f
diptoroy <diptoroy54@gmail.com>
2020-01-18 21:13:09 +0800

    add image cropper

--- app/src/main/java/com/example/huzhouocr/OCRActivity.java
+++ app/src/main/java/com/example/huzhouocr/OCRActivity.java
@@ -1,20 +1,26 @@
 package com.example.huzhouocr;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class OCRActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    private ImageView selectCamera,selectGallery;
+    private ImageView selectCamera,resultView;
     Uri uri;
 
     @Override
@@ -28,9 +34,8 @@ public class OCRActivity extends AppCompatActivity implements View.OnClickListen
     private void init() {
 
         selectCamera = findViewById(R.id.camera);
-        selectGallery = findViewById(R.id.gallery);
+        resultView = findViewById(R.id.result_image);
 
-        selectGallery.setOnClickListener(this);
         selectCamera.setOnClickListener(this);
     }
 
@@ -43,35 +48,45 @@ public class OCRActivity extends AppCompatActivity implements View.OnClickListen
                 sCamera();
                 break;
 
-            case R.id.gallery:
-                sGallery();
-                break;
-
             default:
         }
 
     }
 
-    private void sGallery() {
-
-        //gallery code here
-        CropImage.startPickImageActivity(OCRActivity.this);
-    }
-
     private void sCamera() {
 
         //camera code here
+        CropImage.startPickImageActivity(OCRActivity.this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if (resultCode == RESULT_OK) {
-                uri = result.getUri();
-            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                Exception error = result.getError();
+
+        if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            Uri imageUri = CropImage.getPickImageResultUri(this,data);
+            if (CropImage.isReadExternalStoragePermissionsRequired(this,imageUri)){
+                uri = imageUri;
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},0);
+            }else{
+                startCrop(imageUri);
             }
         }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK){
+                resultView.setImageURI(result.getUri());
+                Toast.makeText(this,"Image update Successfully",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void startCrop(Uri imageUri) {
+
+        CropImage.activity(imageUri)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setMultiTouchEnabled(true)
+                .start(this);
     }
 }
